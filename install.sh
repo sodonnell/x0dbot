514037d649ddd92ebe85b394c4a4adf799db86ad
Sean O'Donnell <sean@seanodonnell.com>
2020-01-25 23:48:18 -0800

    removed Acme::Include from cpanm list

--- install.sh
+++ install.sh
@@ -11,7 +11,6 @@
 #sudo apt install cpanminus
 #sudo yum install cpanminus
 cpanm install Number::Format \
-    Acme::Include \
     XML::RSS::Parser \
     POE::Component::IRC \
     Chatbot::Eliza \
