9e5ae6cc7d9055f82157cadc0beb7ee07763550b
Sean O'Donnell <sean@seanodonnell.com>
2020-01-25 22:54:18 -0800

    cpanm

--- install.sh
+++ install.sh
@@ -1,19 +1,19 @@
 # old-school
-sudo perl -MCPAN -e 'install Number::Format'
-sudo perl -MCPAN -e 'install XML::RSS::Parser'
-sudo perl -MCPAN -e 'install POE::Component::IRC'
-sudo perl -MCPAN -e 'install Chatbot::Eliza'
-sudo perl -MCPAN -e 'install LWP::UserAgent'
-sudo perl -MCPAN -e 'install Regexp::Common'
-sudo perl -MCPAN -e 'install Digest::MD5'
+#sudo perl -MCPAN -e 'install Number::Format'
+#sudo perl -MCPAN -e 'install XML::RSS::Parser'
+#sudo perl -MCPAN -e 'install POE::Component::IRC'
+#sudo perl -MCPAN -e 'install Chatbot::Eliza'
+#sudo perl -MCPAN -e 'install LWP::UserAgent'
+#sudo perl -MCPAN -e 'install Regexp::Common'
+#sudo perl -MCPAN -e 'install Digest::MD5'
 
 # use cpanminus instead. 
-# sudo apt install cpanminus
-# sudo yum install cpanminus
-#cpanm install Number::Format \
-#    XML::RSS::Parser \
-#    POE::Component::IRC \
-#    Chatbot::Eliza \
-#    LWP::UserAgent \
-#    Regexp::Common \
-#    Digest::MD5
+#sudo apt install cpanminus
+#sudo yum install cpanminus
+cpanm install Number::Format \
+    XML::RSS::Parser \
+    POE::Component::IRC \
+    Chatbot::Eliza \
+    LWP::UserAgent \
+    Regexp::Common \
+    Digest::MD5
